517273beb84d077da213bdf615e8951bef8a597f
pro <pro@BIGDOG.lls>
2024-10-15 15:53:12 -0700

    tweak flux noise

--- candle-transformers/src/models/flux/sampling.rs
+++ candle-transformers/src/models/flux/sampling.rs
@@ -94,7 +94,7 @@ pub fn unpack(xs: &Tensor, height: usize, width: usize) -> Result<Tensor> {
 
 fn exponential_decay(total_steps: usize, current_step: usize) -> f64 {
     let k = 10.0; // Controls the steepness of the decay
-    let t = (current_step) as f64 / (total_steps) as f64; // Normalize current step to [0, 1]
+    let t = (current_step) as f64 / (total_steps -1) as f64; // Normalize current step to [0, 1]
     //std::f64::consts::E.powf(-k * t) // Exponential decay formula
     (std::f64::consts::E).powf(-k * t)
 }
@@ -133,24 +133,25 @@ pub fn denoise<M: super::WithForward>(
         };
         let t_vec = Tensor::full(*t_curr as f32, b_sz, dev)?;
 
-        let sigma_down = *t_prev - *t_curr;
+        let sigma_down = *t_curr - *t_prev;
         println!(
-            "flux current step {} - t_curr {} t_prev{}",
+            "**flux current step {} - t_curr {} t_prev{}",
             current_step, t_curr, t_prev
         );
         let decay_value = exponential_decay(timesteps.len(), current_step);
         println!("flux current step {} decay {}", current_step, decay_value);
-        current_step += 1;
+        
         let sigma_down_sqrt = sigma_down;
         println!("sigma_down_sqrt {:?}",sigma_down_sqrt);
-        let stdev = eta * decay_value * sigma_down_sqrt;
+        let stdev = eta * *t_curr;
         println!(
             "flux current step {} flux add noise {:?}",
             current_step, stdev,
-        );
-        let noise = img.randn_like(0.0, stdev)?;
+        );        
+        let noise = (img.randn_like(0.0, stdev)? * sigma_down_sqrt)?;
         img = (img + noise)?;
         let pred = model.forward(&img, img_ids, txt, txt_ids, &t_vec, vec_, Some(&guidance))?;
+        current_step += 1;
         img = (img + pred * (t_prev - t_curr))?
     }
     Ok(img)
