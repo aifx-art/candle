24724be0ac612ddc230cc8c185f8aeee3c0219f5
luke <luke@aifxart>
2024-10-24 22:15:23 -0700

    sd3m

--- candle-examples/examples/stable-diffusion-3/main.rs
+++ candle-examples/examples/stable-diffusion-3/main.rs
@@ -112,13 +112,14 @@ fn run(args: Args) -> Result<()> {
 
     let api = hf_hub::api::sync::Api::new()?;
     let sai_repo = {
-        let name = "stabilityai/stable-diffusion-3.5-large";// "stabilityai/stable-diffusion-3-medium";
+        let name = "stabilityai/stable-diffusion-3-medium";
+        //let name = "stabilityai/stable-diffusion-3.5-large";// "stabilityai/stable-diffusion-3-medium";
         api.repo(hf_hub::Repo::model(name.to_string()))
     };
 
-    //let model_file = sai_repo_3m.get("sd3_medium_incl_clips_t5xxlfp16.safetensors")?;
-    let model_file = sai_repo.get("sd3.5_large.safetensors")?;
-    
+    let model_file = sai_repo.get("sd3_medium_incl_clips_t5xxlfp16.safetensors")?;
+   // let model_file = sai_repo.get("sd3.5_large.safetensors")?;
+
     let vb_fp16 = unsafe {
         candle_nn::VarBuilder::from_mmaped_safetensors(&[model_file.clone()], DType::F16, &device)?
     };
